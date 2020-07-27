131dfb76f453e4e35e11c047603e38e7a7332aed
vejei <chengsnem@163.com>
2020-07-27 17:52:35 +0800

    denpendency injection scopes

---
+++ app/src/main/java/com/example/wanandroid/di/Scopes.kt
@@ -0,0 +1,11 @@
+package com.example.wanandroid.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScoped
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class FragmentScoped
